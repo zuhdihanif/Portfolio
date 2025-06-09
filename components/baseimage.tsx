9a4a7554db6b06d0a44583d29b0142e2c83f1524
Muhammad Zuhdi Hanif <muhammadzuhdihanif@gmail.com>
2025-06-09 22:43:54 +0700

    asset config

--- components/baseimage.tsx
+++ components/baseimage.tsx
@@ -2,9 +2,6 @@
 
 import Image, { ImageProps } from 'next/image'
 
-// Hardcoded sesuai next.config.mjs
-const basePath = process.env.NODE_ENV === 'production' ? '/portfolio' : ''
-
 export default function BaseImage(props: ImageProps) {
-  return <Image {...props} src={basePath + props.src} />
+  return <Image {...props} />
 }
